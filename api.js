16da097af81cf134708daab938f0b5c909f77209
Matt Karl <matt@cloudkid.com>
2014-08-07 17:28:50 -0400

    Auto-generated commit

---
+++ api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Animator",
+        "AnimatorTimeline",
+        "BitmapMovieClip",
+        "BitmapUtils",
+        "Button",
+        "CharacterClip",
+        "CharacterController",
+        "CreateJSDisplay",
+        "DragManager",
+        "Texture",
+        "TextureAtlas"
+    ],
+    "modules": [
+        "cloudkid"
+    ],
+    "allModules": [
+        {
+            "displayName": "cloudkid",
+            "name": "cloudkid"
+        }
+    ]
+} };
+});
